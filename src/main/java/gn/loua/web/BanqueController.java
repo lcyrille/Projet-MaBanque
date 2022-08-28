a69d2b8009eb70ba3286ea31298ee2685837df14
lcyrille <louacyrille90@gmail.com>
2022-08-28 02:24:19 +0000

    Mise à jour du 28/08/2022

--- src/main/java/gn/loua/web/BanqueController.java
+++ src/main/java/gn/loua/web/BanqueController.java
@@ -2,10 +2,13 @@ package gn.loua.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import gn.loua.dao.OperationRepository;
 import gn.loua.entities.Compte;
 import gn.loua.entities.Operation;
 import gn.loua.metier.IBanqueMetier;
@@ -22,13 +25,19 @@ public class BanqueController {
 	}
 	
 	@RequestMapping("/consulterCompte")
-	public String consulter(Model model, String codeCompte) {
+	public String consulter(Model model, String codeCompte,
+			@RequestParam(name="page", defaultValue="0")int p,
+			@RequestParam(name="size", defaultValue="2")int s) {
 		model.addAttribute("codeCompte", codeCompte);
 		try {
 			Compte cp=banqueMetier.consulterCompte(codeCompte);
-			Page<Operation> pageOperations=banqueMetier.listOperation(codeCompte,0,4);
+			Page<Operation> pageOperations=banqueMetier.listOperation(codeCompte,p,s);
 			model.addAttribute("listeOperations",pageOperations.getContent());
 			model.addAttribute("compte",cp);
+			int[] pages=new int[pageOperations.getTotalPages()];
+			model.addAttribute("pages",pages);
+			model.addAttribute("size",s);
+			model.addAttribute("pageCourante",p);
 		} catch (Exception e){
 			model.addAttribute("exception",e);
 		}
